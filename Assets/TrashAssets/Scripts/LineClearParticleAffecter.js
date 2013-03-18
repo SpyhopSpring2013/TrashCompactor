a7c6916f6919ef0b0e3ceee5d73ccc6646a1c56b
Mathew Beseris <matbeseris@aol.com>
2013-03-18 17:46:48 -0600

    Changed Particle Affects and Made The Style of Particle Affect Work

--- Assets/TrashAssets/Scripts/LineClearParticleAffecter.js
+++ Assets/TrashAssets/Scripts/LineClearParticleAffecter.js
@@ -1,4 +1,8 @@
-public var sparkEffect: GameObject;
+public var sparkEffectGlass: GameObject;
+public var sparkEffectPaper: GameObject;
+public var sparkEffectPlastic: GameObject;
+
+
 
 public var uiManager:UIManager;
 
@@ -8,10 +12,26 @@ public function onClearLine(row:int, style:int)
 	//uiManager.posOffset; //vector3
 	//uiManager.blockSize; //vector3
 
-	Instantiate(sparkEffect, Vector3(uiManager.posOffset.x+uiManager.blockSize.x*10/2,
+	//Glass = 1 Paper = 2 Plastic = 3
+
+	if(style == 1){
+	Instantiate(sparkEffectGlass, Vector3(uiManager.posOffset.x+uiManager.blockSize.x*10/2,
 									uiManager.posOffset.y + uiManager.blockSize.y*row + uiManager.blockSize.y/2,
 									uiManager.posOffset.z),
 									Quaternion.identity);
+	}
+	if(style == 2){
+	Instantiate(sparkEffectPaper, Vector3(uiManager.posOffset.x+uiManager.blockSize.x*10/2,
+									uiManager.posOffset.y + uiManager.blockSize.y*row + uiManager.blockSize.y/2,
+									uiManager.posOffset.z),
+									Quaternion.identity);
+	}
+	if(style == 3){
+	Instantiate(sparkEffectPlastic, Vector3(uiManager.posOffset.x+uiManager.blockSize.x*10/2,
+									uiManager.posOffset.y + uiManager.blockSize.y*row + uiManager.blockSize.y/2,
+									uiManager.posOffset.z),
+									Quaternion.identity);
+	}
 
 	//myParticleEffect.Play();
 	/* myParticleEffect.emit = true;
